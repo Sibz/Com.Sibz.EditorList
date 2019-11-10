c74e7ddd0d49f173e288c3cdb3f8900b4485f7ac
Steven Turner <steven@entityzero.com>
2019-11-10 13:48:08 +1100

    Re organised ListDrawer class into partial files

---
+++ Editor/ListDrawer.Methods.cs
@@ -0,0 +1,73 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Sibz.EditorList.Editor
+{
+    /// <summary>
+    /// Base class to derive from when making nice editor lists.
+    /// </summary>
+    /// <typeparam name="T">Type of item in the list</typeparam>
+    public abstract partial class ListDrawer<T> : PropertyDrawer
+    {
+        /// <summary>
+        /// Clears the list property.
+        /// <para>Can be overridden if not altering property directly or other logic is required.</para>
+        /// </summary>
+        /// <param name="property">The main SerializedProperty the list belongs to</param>
+        /// <param name="listProperty">The list SerializedProperty that has the associated array attached.</param>
+        protected virtual void ClearList(SerializedProperty listProperty) => listProperty.ClearArray();
+
+        /// <summary>
+        /// Deletes an item from the list property.
+        /// <para>Can be overridden if not altering property directly or other logic is required.</para>
+        /// </summary>
+        /// <param name="listProperty"></param>
+        /// <param name="listItemProperty"></param>
+        /// <param name="index"></param>
+        protected virtual void DeleteItem(SerializedProperty listProperty, SerializedProperty listItemProperty, int index)
+        {
+            listProperty.DeleteArrayElementAtIndex(index);
+        }
+
+        /// <summary>
+        /// Moves an item within the list property. Is only called if toIndex is valid (I.e. not outside the list).
+        /// <para>Can be overridden if not altering property directly or other logic is required.</para>
+        /// </summary>
+        /// <param name="listProperty"></param>
+        /// <param name="listItemProperty"></param>
+        /// <param name="fromIndex"></param>
+        /// <param name="toIndex"></param>
+        protected virtual void MoveTo(SerializedProperty listProperty, SerializedProperty listItemProperty, int fromIndex, int toIndex)
+        {
+            listProperty.MoveArrayElement(fromIndex, toIndex);
+        }
+
+        /// <summary>
+        /// Checks item index and calls MoveTo
+        /// </summary>
+        /// <param name="listProperty"></param>
+        /// <param name="listItemProperty"></param>
+        /// <param name="fromIndex"></param>
+        private void MoveUp(SerializedProperty listProperty, SerializedProperty listItemProperty, int fromIndex)
+        {
+            if (fromIndex > 0)
+            {
+                MoveTo(listProperty, listItemProperty, fromIndex, fromIndex - 1);
+            }
+        }
+
+        /// <summary>
+        /// Checks item index and calls MoveTo
+        /// </summary>
+        /// <param name="listProperty"></param>
+        /// <param name="listItemProperty"></param>
+        /// <param name="fromIndex"></param>
+        private void MoveDown(SerializedProperty listProperty, SerializedProperty listItemProperty, int fromIndex)
+        {
+            if (fromIndex < listProperty.arraySize)
+            {
+                MoveTo(listProperty, listItemProperty, fromIndex, fromIndex + 1);
+            }
+        }
+    }
+}
---
+++ Editor/ListDrawer.Sections.cs
@@ -0,0 +1,126 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Sibz.EditorList.Editor
+{
+    /// <summary>
+    /// Base class to derive from when making nice editor lists.
+    /// </summary>
+    /// <typeparam name="T">Type of item in the list</typeparam>
+    public abstract partial class ListDrawer<T> : PropertyDrawer
+    {
+        protected virtual void ContentSection(GUIContent label)
+        {
+
+            var listProperty = Property.FindPropertyRelative(nameof(EditorList<T>.List));
+            if (listProperty != null)
+            {
+                Header(listProperty, label);
+
+                ListAreaSection(listProperty);
+
+                Footer(listProperty);
+            }
+            else
+            {
+                Debug.LogWarning($"{nameof(ListDrawer<T>)}: Unable to get list property. Be sure your property extends EditorList<T>.");
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="listProperty"></param>
+        protected virtual void ListAreaSection(SerializedProperty listProperty)
+        {
+            for (int i = 0; i < listProperty.arraySize; i++)
+            {
+                var listItemProperty = listProperty.GetArrayElementAtIndex(i);
+                ListItemAreaDrawer(listProperty, listItemProperty, i);
+            }
+        }
+
+        /// <summary>
+        /// Header section. Can be overriden to change what content appears above the list.
+        /// </summary>
+        /// <param name="listProperty">The list SerializedProperty that has the associated array attached.</param>
+        /// <param name="label">Label provided to the main PropertyField</param>
+        protected virtual void Header(SerializedProperty listProperty, GUIContent label) { }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="listProperty"></param>
+        /// <param name="listItemProperty"></param>
+        /// <param name="index"></param>
+        protected virtual void ListItemAreaDrawer(SerializedProperty listProperty, SerializedProperty listItemProperty, int index)
+        {
+            EditorGUILayout.BeginHorizontal();
+            {
+                ListItemDrawer(listProperty, listItemProperty, index);
+
+                ListItemButtonsDrawer(listProperty, listItemProperty, index);
+
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        /// <summary>
+        /// Item section. Defaults to a Property Field. Override if required. Is drawn inside Horizontal Section with buttons.
+        /// </summary>
+        /// <param name="listProperty">The list SerializedProperty that has the associated array attached.</param>
+        /// <param name="listItemProperty">The listItem SerializedProperty</param>
+        /// <param name="index">Index of item</param>
+        protected virtual void ListItemDrawer(SerializedProperty listProperty, SerializedProperty listItemProperty, int index)
+        {
+            EditorGUILayout.PropertyField(listItemProperty, GUIContent.none);
+        }
+
+        /// <summary>
+        /// Item Buttons Section. Defaults to Delete button and if Ordered=true, MoveUp and MoveDown  buttons.
+        /// Is drawn inside Horizontal Section with list item.
+        /// </summary>
+        /// <param name="listProperty">The list SerializedProperty that has the associated array attached.</param>
+        /// <param name="listItemProperty">The listItem SerializedProperty</param>
+        /// <param name="index">Index of item</param>
+        protected virtual void ListItemButtonsDrawer(SerializedProperty listProperty, SerializedProperty listItemProperty, int index)
+        {
+            if (DeleteButton)
+            {
+                DeleteItem(listProperty, listItemProperty, index);
+            }
+            if (Ordered)
+            {
+                if (MoveUpButton)
+                {
+                    MoveUp(listProperty, listItemProperty, index);
+                }
+
+                if (MoveDownButton)
+                {
+                    MoveDown(listProperty, listItemProperty, index);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Footer section. Defaults to a Delete All Button.
+        /// Can be overridden to display other content, or remove the button.
+        /// </summary>
+        /// <param name="property">The main SerializedProperty the list belongs to</param>
+        /// <param name="listProperty">The list SerializedProperty that has the associated array attached.</param>
+        protected virtual void Footer(SerializedProperty listProperty)
+        {
+            EditorGUILayout.BeginHorizontal();
+            {
+                int indent = IndentLevelChange + (IndentContent ? 1 : 0);
+                GUILayout.Label("", GUILayout.MaxWidth(indent * 10));
+                if (DeleteAllButton)
+                {
+                    ClearList(listProperty);
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
